4a620576e0dccc8a1f9a401b3d4bc3b13baadc5c
Oliver Spring <oliver9589@gmail.com>
2017-10-02 17:34:51 +0000

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,32 @@
 // define your actions here!
+export function addMovie(movie) {
+  return {
+    type: 'ADD_MOVIE',
+    movie: {
+      title: movie.title
+    }
+  }
+}
+
+export function deleteMovie(movie) {
+  return {
+    type: 'DELETE_MOVIE',
+    movie: {
+      title: movie.title
+    }
+  }
+}
+
+export function updateUsername(newName) {
+  return {
+    type: 'UPDATE_USERNAME',
+    username: newName
+  }
+}
+
+export function resetUsername() {
+  return {
+    type: 'RESET_USERNAME',
+    username: 'default'
+  }
+}
